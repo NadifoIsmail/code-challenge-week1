3aeea3bebfe556a78a1c34e8900f5e58a0909ded
Nadifo Ismail <nadifoismail80@gmail.com>
2024-09-29 21:55:56 +0300

    modifies challenge-2.js

--- challenge-2.js
+++ challenge-2.js
@@ -4,19 +4,21 @@
 const prompt = require("prompt-sync")();
 
 //function to check the speed of a car and determine their demerit points or license suspension
-function checkSpeed(speed){
-const speedLimit = 70;
-const kmPerPoint= 5;
-//if the speed is less than the speedLimit it prints Ok
-if(speed < 70){
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  let points = 0;
+  //if the speed is less than the speedLimit it prints Ok
+  if (speed < 70) {
     console.log("Ok");
-}
-else {
-    
-
-    if(points > 12){
-        console.log("License suspended"); 
-}
+  } else {
+    for (let i = speedLimit; i < speed; i += 5) {
+      points++;
+    }
+    console.log(`Points:${points}`);
+  }
+  if (points > 12) {
+    console.log("License suspended");
+  }
 }
 //Allow the user to enter the speed of the car
 let speed = prompt("Enter the speed :");
--- challenge-3.js
+++ challenge-3.js
@@ -10,88 +10,68 @@ const grossSalary = basicSalary + benefits;
 
 //function determining the payee
 function payee() {
-	let payeeAmount;
-    if (grossSalary <= 24000) {
-        payeeAmount = grossSalary * 0.10;
-    } 
-	else if (grossSalary >= 24001 && grossSalary <= 32333) {
-        payeeAmount = grossSalary * 0.25;
-    } 
-	else if (grossSalary >= 32334 && grossSalary <= 500000) {
-        payeeAmount = grossSalary * 0.30;
-    } 
-	else if (grossSalary >= 500001 && grossSalary <=800000) {
-        payeeAmount = (grossSalary - 60000) * 0.25;
-    }
-	else {
-        payeeAmount = grossSalary * 0.35;
-    }
-    return payeeAmount;
+  let payeeAmount;
+  if (grossSalary <= 24000) {
+    payeeAmount = grossSalary * 0.1;
+  } else if (grossSalary >= 24001 && grossSalary <= 32333) {
+    payeeAmount = grossSalary * 0.25;
+  } else if (grossSalary >= 32334 && grossSalary <= 500000) {
+    payeeAmount = grossSalary * 0.3;
+  } else if (grossSalary >= 500001 && grossSalary <= 800000) {
+    payeeAmount = (grossSalary - 60000) * 0.25;
+  } else {
+    payeeAmount = grossSalary * 0.35;
+  }
+  return payeeAmount;
 }
 
-//function for determining NHIF deductions 
+//function for determining NHIF deductions
 
 function nhifDeductions() {
-    let deductionAmount;
-    if (grossSalary <= 5999) {
-        deductionAmount = 150;
-    } 
-    else if (grossSalary >= 6000 && grossSalary <= 7999) {
-        deductionAmount = 300;
-    } 
-    else if (grossSalary >= 8000 && grossSalary <= 11999) {
-        deductionAmount = 400;
-    } 
-    else if (grossSalary >= 12000 && grossSalary <=14999){
-        deductionAmount = 500;
-    }
-	else if (grossSalary >= 15000 && grossSalary <=19999) {
-		deductionAmount = 600;
-	}
-	else if (grossSalary >= 20000 && grossSalary <=24999) {
-        deductionAmount = 750;
-    }
-	else if (grossSalary >= 25000 && grossSalary <=29999) {
-		deductionAmount = 850;
-	}
-	else if (grossSalary >= 30000 && grossSalary <=34999) {
-		deductionAmount = 900;
-	}
-	else if (grossSalary >= 35000 && grossSalary <=39999) {
-		deductionAmount = 950;
-	}
-	else if (grossSalary >= 40000 && grossSalary <=44999) {
-		deductionAmount = 1000;
-	}
-	else if (grossSalary >= 45000 && grossSalary <=49999) {
-		deductionAmount = 1100;
-	}
-	else if (grossSalary >= 50000 && grossSalary <=59999) {
-		deductionAmount = 1200;
-	}
-	else if (grossSalary >= 60000 && grossSalary <=69999) {
-		deductionAmount = 1300;
-	}
-	else if (grossSalary >= 70000 && grossSalary <=79999) {
-		deductionAmount = 1400;
-	}
-	else if (grossSalary >= 80000 && grossSalary <=89999) {
-		deductionAmount = 1500;
-	}
-	else if (grossSalary >= 90000 && grossSalary <=99999) {
-		deductionAmount = 1600;
-	}
-	else {
-		deductionAmount = 1700;
-	}
-    return deductionAmount;
+  let deductionAmount;
+  if (grossSalary <= 5999) {
+    deductionAmount = 150;
+  } else if (grossSalary >= 6000 && grossSalary <= 7999) {
+    deductionAmount = 300;
+  } else if (grossSalary >= 8000 && grossSalary <= 11999) {
+    deductionAmount = 400;
+  } else if (grossSalary >= 12000 && grossSalary <= 14999) {
+    deductionAmount = 500;
+  } else if (grossSalary >= 15000 && grossSalary <= 19999) {
+    deductionAmount = 600;
+  } else if (grossSalary >= 20000 && grossSalary <= 24999) {
+    deductionAmount = 750;
+  } else if (grossSalary >= 25000 && grossSalary <= 29999) {
+    deductionAmount = 850;
+  } else if (grossSalary >= 30000 && grossSalary <= 34999) {
+    deductionAmount = 900;
+  } else if (grossSalary >= 35000 && grossSalary <= 39999) {
+    deductionAmount = 950;
+  } else if (grossSalary >= 40000 && grossSalary <= 44999) {
+    deductionAmount = 1000;
+  } else if (grossSalary >= 45000 && grossSalary <= 49999) {
+    deductionAmount = 1100;
+  } else if (grossSalary >= 50000 && grossSalary <= 59999) {
+    deductionAmount = 1200;
+  } else if (grossSalary >= 60000 && grossSalary <= 69999) {
+    deductionAmount = 1300;
+  } else if (grossSalary >= 70000 && grossSalary <= 79999) {
+    deductionAmount = 1400;
+  } else if (grossSalary >= 80000 && grossSalary <= 89999) {
+    deductionAmount = 1500;
+  } else if (grossSalary >= 90000 && grossSalary <= 99999) {
+    deductionAmount = 1600;
+  } else {
+    deductionAmount = 1700;
+  }
+  return deductionAmount;
 }
 
-//function for determining NSSF deduction 
+//function for determining NSSF deduction
 
 function nssfDeductions() {
-	let nssf = grossSalary * 0.06;
-	return nssf;
+  let nssf = grossSalary * 0.06;
+  return nssf;
 }
 let nssf = nssfDeductions();
 let nhif = nhifDeductions();
